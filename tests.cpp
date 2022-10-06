60bfbd7884b46c8b7cf3e2959406215c119fc65c
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-10-06 15:02:52 +0800

    Initial commit

---
+++ tests.cpp
@@ -0,0 +1,37 @@
+// include
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+
+string greetMsg = "Hello world";
+
+cout << greetMsg << "\n";
+
+cout << "greet \n\n";
+
+cout << "ABC" << endl;
+
+
+  
+  
+int x, y;
+int sum;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+sum = x + y;
+cout << "Sum is: " << sum << "\n";
+
+
+
+// press any key to continue
+system("pause");
+
+
+
+// return
+return 0;
+}
